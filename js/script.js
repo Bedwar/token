02384aeac956b03cc64f4cb39121240a9412d003
Bedwar <31661075+Bedwar@users.noreply.github.com>
2021-09-20 00:23:58 -0300

    scripst

--- js/script.js
+++ js/script.js
@@ -1,37 +1,21 @@
 
+document.querySelector("#ibm").addEventListener('change',soli)
+document.querySelector("#pc").addEventListener('change',soli)
+document.querySelector("#tc").addEventListener('change',soli)
+document.querySelector("#pd").addEventListener('change',soli)
+document.querySelector("#td").addEventListener('change',soli)
+document.querySelector("#ads").addEventListener('change',soli)
+document.querySelector("#acr").addEventListener('change',soli)
 
-function solicitar(){
-	
-
-	var ibm = document.getElementById("ibm");
-	var pcomum = document.getElementById("pcomum");
-	var pdual = document.getElementById("pdual");
-	var tcomum = document.getElementById("tcomum");
-	var tdual = document.getElementById("tdual");
-	var adesivos = document.getElementById("adesivos");
-
-	var dados = JSON.parse(localStorage.getItem("dadosSolicitacao"));
-
-	if (dados == null){
-		localStorage.setItem("dadosSolicitacao","[]");
-		dados = [];
-	}
-
-	var registro = {
-		ibm: ibm.value,
-		pcomum: pcomum.value,
-		pdual: pdual.value,
-		tcomum: tcomum.value,
-		tdual: tdual.value,
-		adesivos: adesivos.value
-	}
-
-	dados.push(registro);
-
-	localStorage.setItem("dadosSolicitacao",JSON.stringify(dados));
-	alert("Pendencia Enviada");
-
-		dados.value = "";
-		window.location.href = "index.html";
-		
+function soli(){
+const ibm = document.querySelector("#ibm").value
+const pc = document.querySelector("#pc").value
+const tc = document.querySelector("#tc").value
+const pd = document.querySelector("#pd").value
+const td = document.querySelector("#td").value
+const ads = document.querySelector("#ads").value
+const acr = document.querySelector("#acr").value
+let pen = "IBM: " + ibm + " <br> P COMUM: " + pc + " TCOMUM: " + tc + " <br> P DUAL: " + pd + " TDUAL: " + td + "  <br> ADESIVOS: " + ads + " <br> ACRILICO: "  + acr
+document.querySelector("#sol").innerHTML =  pen
 }
+
